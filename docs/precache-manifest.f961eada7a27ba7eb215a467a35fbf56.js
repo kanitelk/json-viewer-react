4000697048d20831e81eb9572e4496d9df0d7721
Kanitelk <derodroll@gmail.com>
2019-12-14 17:50:38 +0300

    base url path fix

---
+++ docs/precache-manifest.f961eada7a27ba7eb215a467a35fbf56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20419ae215ae97c104f99ba6c68478ee",
+    "url": "/json-viewer-react/index.html"
+  },
+  {
+    "revision": "7a998c85f78de4bd0794",
+    "url": "/json-viewer-react/static/css/main.4a3bebdf.chunk.css"
+  },
+  {
+    "revision": "981d98a0ba7134860589",
+    "url": "/json-viewer-react/static/js/2.7a48f6ec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/json-viewer-react/static/js/2.7a48f6ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a998c85f78de4bd0794",
+    "url": "/json-viewer-react/static/js/main.f886934b.chunk.js"
+  },
+  {
+    "revision": "c2906dd475193106963a",
+    "url": "/json-viewer-react/static/js/runtime-main.e61eae20.js"
+  }
+]);
